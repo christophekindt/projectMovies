15401134ae685322af22062a59995b49156a7fce
christophekindt <christophe.kindt@telenet.be>
2015-12-08 10:27:41 +0100

    Initial Commit

---
+++ ProjectFilmsTests/ProjectFilmsTests.swift
@@ -0,0 +1,36 @@
+//
+//  ProjectFilmsTests.swift
+//  ProjectFilmsTests
+//
+//  Created by Christophe Kindt on 08/12/15.
+//  Copyright © 2015 Christophe Kindt. All rights reserved.
+//
+
+import XCTest
+@testable import ProjectFilms
+
+class ProjectFilmsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
